08cde9484b0678b37ce7b3c53199f35ebb81971a
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2023-07-07 15:43:56 -0400

    added api call and match/case logic to app.py

--- app.py
+++ app.py
@@ -11,7 +11,27 @@ def index():
 
 @app.route('/news_source/<news>')
 def news_source(news):
-    return "This is " + str(news)
+
+    # TODO: This line of code should instead direct user
+    #  to the respectivenews webpage
+    # perhaps most recent article
+
+    # for now, the user cannot choose their news source dynamically.
+    # Will need a web scraping tool or something to get this.
+    # for now, will allow user three options. For MVP...
+
+    # https://developer.nytimes.com/apis - ny times api page
+    nyt_api_call = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={6CIcTlf5BAmG5u4ttYxeOGAFlAWJhXFr}'
+    atlantic_api_call = '#'
+    sf_api_call = '#'
+    match news:
+        case 'The New York Times':
+            return nyt_api_call
+        case 'The Atlantic':
+            return atlantic_api_call
+        case 'The San Francisco Chronicle':
+            return sf_api_call 
+    return 
 
 
 if __name__ == '__main__':
